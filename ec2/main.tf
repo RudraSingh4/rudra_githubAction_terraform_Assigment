bba6ab4a7a21a7529ea23d7be5be428bda9ebc0f
Rudra <rishurudrasingh@gmail.com>
2023-03-17 15:49:53 +0530

    first commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,17 @@
 
 # Create  Public subnet
-resource "aws_subnet" "rudra_public_subnet_1" {
-    vpc_id = aws_vpc.rudra_vpc.id
-    cidr_block = var.subnet_cidr
-    availability_zone = "ap-south-1"
+# resource "aws_subnet" "rudra_public_subnet_1" {
+#     vpc_id = aws_vpc.rudra_vpc.id
+#     cidr_block = var.subnet_cidr
+#     availability_zone = "ap-south-1"
 
-    tags = {
-      Name = "rudra_public_subnet_1"
-      owner = "rudra.singh@cloudeq.com"
-      purpose = "training"
-    }
+#     tags = {
+#       Name = "rudra_public_subnet_1"
+#       owner = "rudra.singh@cloudeq.com"
+#       purpose = "training"
+#     }
   
-}
+# }
 
 
 resource "aws_instance" "name" {
